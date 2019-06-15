0b3f3f28675e2ab24149ffd4a48cbad2b9c98cb9
Toshiyuki-Takahashi <dellerba0110@gmail.com>
2019-06-16 06:38:53 +0900

    first commit

---
+++ m_cal.php
@@ -0,0 +1,19 @@
+<?php
+if (!empty($_POST['sex']) && !empty($_POST['weight'])) {
+	$sex = $_POST['sex'];
+	$weight = $_POST['weight'];
+} else {
+	echo 'データが入力されていません、再度入力し直してください<br>';
+	echo '<a href="./m_form.php">入力画面へ戻る</a>';
+}
+
+//メッセージ入れる
+//フォーム画面リンク入れる
+if ($sex == '男') {
+	$result = $weight * 40;
+	echo $result;
+} else {
+	$result = $weight * 30;
+	echo $result;
+}
+?>
