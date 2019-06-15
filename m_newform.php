cc990eaaf8ee2098da1be400b4f6a9b5bcc28cda
Toshiyuki-Takahashi <dellerba0110@gmail.com>
2019-06-16 06:39:52 +0900

    first commit

---
+++ m_newform.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>基礎代謝計算フォーム</title>
+</head>
+<body>
+<form method="POST" action="">
+<p>性別を選んでください<br>
+<input type="radio" name="sex" value="男">男
+<input type="radio" name="sex" value="女">女
+</p>
+<p>体重を入力してください<br>
+<input type="num1" name="weight">kg
+</p>
+<p>目的を選んでください<br>
+<input type="radio" name="controll" value="増量">増量
+<input type="radio" name="controll" value="維持">維持
+<input type="radio" name="controll" value="減量">減量
+</p>
+<input type="submit" value="入力">
+</form>
+</body>
+</html>
